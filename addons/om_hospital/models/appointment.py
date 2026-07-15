f907d356b304e6cd04d0c557b6bb0a944ab435d2
aufall02 <aufaluzumaki@gmail.com>
2026-07-15 11:15:47 +0700

    Date And Datetime Fields

--- addons/om_hospital/models/appointment.py
+++ addons/om_hospital/models/appointment.py
@@ -5,8 +5,9 @@ from odoo import models, fields
 
 class HospitalAppointment(models.Model):
     _name = 'hospital.appointment'
-    _inherit = ['mail.thread','mail.activity.mixin']
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Hospital Appointment'
 
-
     patient_id = fields.Many2one(comodel_name='hospital.patient', string='Patient')
+    appointment_time = fields.Datetime(string='Appointment Time')
+    booking_date = (fields.Date(string='Booking Date'))

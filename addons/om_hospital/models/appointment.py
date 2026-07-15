e908cdc76a5d5527d1d4e0246c34ef569d22883c
aufall02 <aufaluzumaki@gmail.com>
2026-07-15 13:41:10 +0700

    23 Related field

--- addons/om_hospital/models/appointment.py
+++ addons/om_hospital/models/appointment.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from jinja2 import defaults
 from odoo import models, fields
+from odoo.api import readonly
 
 
 class HospitalAppointment(models.Model):
@@ -9,5 +10,6 @@ class HospitalAppointment(models.Model):
     _description = 'Hospital Appointment'
 
     patient_id = fields.Many2one(comodel_name='hospital.patient', string='Patient')
+    gender = fields.Selection(related='patient_id.gender', string='Gender', readonly=False)
     appointment_time = fields.Datetime(string='Appointment Time', default=fields.Datetime.now)
     booking_date = fields.Date(string='Booking Date', default=fields.Date.context_today)

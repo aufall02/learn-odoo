4e95f9507ef7e1786a9475a174e779abc01fad1a
Aufal marom <91175425+aufall02@users.noreply.github.com>
2026-07-20 10:35:02 +0700

    bug

--- addons/training/wizards/training_wizard.py
+++ addons/training/wizards/training_wizard.py
@@ -1,7 +1,7 @@
 from odoo import models, fields
 
 
-class TrainingWizard(models.Model):
+class TrainingWizard(models.TransientModel):
     _name = 'training.wizard'
     _description = 'Training wizard'
 
@@ -9,7 +9,7 @@ class TrainingWizard(models.Model):
         return self.env['training.session'].browse(self._context.get('active_ids'))
 
     session_id = fields.Many2one(comodel_name='training.session', string='Session Training', default=_default_session)
-    participant_ids = fields.Many2many(comodel_name='participant', string='Peserta Training')
+    participant_ids = fields.Many2many(comodel_name='training.participant', string='Peserta Training')
 
-def add_participant(self):
-    self.session_id.participant_ids |= self.participant_ids 
+    def add_participant(self):
+        self.session_id.participant_ids |= self.participant_ids 

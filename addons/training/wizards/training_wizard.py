0fc27019be21143d2d51d882418838ba419197fd
Aufal marom <91175425+aufall02@users.noreply.github.com>
2026-07-20 11:01:22 +0700

    add multi trianing sesion wizard

--- addons/training/wizards/training_wizard.py
+++ addons/training/wizards/training_wizard.py
@@ -10,7 +10,13 @@ class TrainingWizard(models.TransientModel):
 
     session_id = fields.Many2one(comodel_name='training.session', string='Session Training', default=_default_session)
     participant_ids = fields.Many2many(comodel_name='training.participant', string='Peserta Training')
+    session_ids = fields.Many2many(comodel_name='training.session', string='Multi Sesi Training', default=_default_session)
 
     def add_participant(self):
         self.session_id.participant_ids |= self.participant_ids
         return {"type": "ir.actions.act_window_close"} 
+    
+    def tambah_banyak_peserta(self):
+        for session in self.session_ids:
+            session.peserta_ids |= self.peserta_ids
+

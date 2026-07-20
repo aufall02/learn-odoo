3940db27e82dce6be57e5235cbad3a0435ea7777
Aufal marom <91175425+aufall02@users.noreply.github.com>
2026-07-20 13:56:07 +0700

    fix typo

--- addons/om_hospital/wizard/cancel_appointment.py
+++ addons/om_hospital/wizard/cancel_appointment.py
@@ -1,8 +1,7 @@
 from odoo import fields, models
 
 class CancelAppointmentWizard(models.Model):
-    _name = "cancel.appoinment.wizard"
+    _name = "cancel.appointment.wizard"
     _description = "Cancel appointment wizard"\
 
-    appointment_id = fields.Many2one('hospital.apppointment', string='Appointment')
-    
+    appointment_id = fields.Many2one('hospital.appointment', string='Appointment')

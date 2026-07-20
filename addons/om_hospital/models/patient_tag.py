f86e1f4a38748df2de658c4ff7e61e4cd2d60459
Aufal marom <91175425+aufall02@users.noreply.github.com>
2026-07-20 09:40:12 +0700

    patient tag

---
+++ addons/om_hospital/models/patient_tag.py
@@ -0,0 +1,8 @@
+from odoo import models, fields, api
+
+class PatientTag(models.Model):
+    _name = 'patient.tag'
+    _description = 'Patient Tag'
+
+    name = fields.Char(string="Name", required=True)
+    active = fields.Boolean(string="Active", default=True)

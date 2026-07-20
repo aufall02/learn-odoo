2c3e26ab3c060e1b973021fefe2072920b9bad8a
Aufal marom <91175425+aufall02@users.noreply.github.com>
2026-07-20 11:15:33 +0700

    typo integer

--- addons/om_hospital/models/patient_tag.py
+++ addons/om_hospital/models/patient_tag.py
@@ -6,4 +6,4 @@ class PatientTag(models.Model):
 
     name = fields.Char(string="Name", required=True)
     active = fields.Boolean(string="Active", default=True)
-    color = fields.Interger(string="Color")
+    color = fields.Integer(string="Color")

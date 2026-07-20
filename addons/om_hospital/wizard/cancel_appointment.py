97539a8c5a1dca3ef392085ea8e5f6694154a926
Aufal marom <91175425+aufall02@users.noreply.github.com>
2026-07-20 13:56:28 +0700

    remove \

--- addons/om_hospital/wizard/cancel_appointment.py
+++ addons/om_hospital/wizard/cancel_appointment.py
@@ -2,6 +2,6 @@ from odoo import fields, models
 
 class CancelAppointmentWizard(models.Model):
     _name = "cancel.appointment.wizard"
-    _description = "Cancel appointment wizard"\
+    _description = "Cancel appointment wizard"
 
     appointment_id = fields.Many2one('hospital.appointment', string='Appointment')

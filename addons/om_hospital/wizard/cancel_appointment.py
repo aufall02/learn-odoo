381c7a038e9fbf85bd3a09068e6990f995235bfb
aufall02 <aufaluzumaki@gmail.com>
2026-07-21 07:11:09 +0700

    74 get function

--- addons/om_hospital/wizard/cancel_appointment.py
+++ addons/om_hospital/wizard/cancel_appointment.py
@@ -1,12 +1,24 @@
-from odoo import fields, models
+from odoo import fields, models, api
+import datetime
+
 
 class CancelAppointmentWizard(models.Model):
     _name = "cancel.appointment.wizard"
     _description = "Cancel appointment wizard"
 
+    @api.model
+    def default_get(self, fields):
+        print('get function')
+        res = super(CancelAppointmentWizard, self).default_get(fields)
+        res['cancel_date'] = datetime.date.today()
+        return res
+
     appointment_id = fields.Many2one('hospital.appointment', string='Appointment')
     reason = fields.Text(string='Reason')
+    cancel_date = fields.Date(string='Cancel Date')
 
 
     def action_cancel(self):
         return
+
+
